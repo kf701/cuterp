c5bbf27143d6be3918460807e1950dc3355bce7b
root <869516085@qq.com>
2018-02-28 18:05:12 +0800

    fix

--- cuterp_server.py
+++ cuterp_server.py
@@ -1,29 +1,19 @@
-#!/usr/bin/python
+# -*- coding:utf-8 -*-
 
+import os
 import socket
 import select
 import time
 import sys
 import threading
+from struct import *
 
-from BaseHTTPServer import BaseHTTPRequestHandler
-from StringIO import StringIO
+from http.server import BaseHTTPRequestHandler
+from io import StringIO
 
-buffer_size = 4096
-delay = 0.0001
-
+g_sock_name = {}
+g_sock_sock = {}
 g_dev_socks = {}
-g_web_socks = []
-
-def del_dev_sock(sock) :
-    name = None
-    for k,v in g_dev_socks.iteritems():
-        if v == sock :
-            name = k 
-            break
-
-    if not name : return 
-    del g_dev_socks[name]
 
 
 class HTTPRequest(BaseHTTPRequestHandler):
@@ -38,76 +28,12 @@ class HTTPRequest(BaseHTTPRequestHandler):
         self.error_message = message
 
 
-class TheServer:
-    input_list = []
-    channel = {}
-
-    def __init__(self, host, port):
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server.bind((host, port))
-        self.server.listen(200)
-        print 'Listen on', port
-
-    def main_loop(self):
-        self.input_list.append(self.server)
-        while 1:
-            time.sleep(delay)
-            inputready, outputready, exceptready = select.select(self.input_list, [], [])
-            for self.s in inputready:
-                if self.s == self.server:
-                    self.on_accept()
-                    break
-
-                self.data = self.s.recv(buffer_size)
-                if len(self.data) == 0:
-                    self.on_close()
-                    break
-                else:
-                    self.on_recv()
-
-    def on_accept(self):
-        clientsock, clientaddr = self.server.accept()
-        print clientaddr, "has connected"
-        self.input_list.append(clientsock)
-        print 'input list len =', len(self.input_list)
-
-    def on_close(self):
-        print 'on close'
-        #print self.s.getpeername(), "has disconnected"
-        self.input_list.remove(self.s)
-        self.s.close()
-
-        if self.s in self.channel :
-            out = self.channel[self.s]
-            self.input_list.remove(out)
-            out.close()
-            del self.channel[out]
-            del self.channel[self.s]
-        else :
-            print 'not in channel'
-
-    def on_recv(self):
-        if not self.s in self.channel :
-            forward = get_idle_sock()
-            if forward :
-                self.input_list.append(forward)
-                self.channel[self.s] = forward
-                self.channel[forward] = self.s
-        # here we can parse and/or modify the data before send forward
-        #print data
-        #request = HTTPRequest(data)
-        self.channel[self.s].sendall(self.data)
-
-def parse_dev_data(data) :
-
-
-def dev_loop() :
+def web_loop():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind(('', 5800))
+    server.bind(('', 5801))
     server.listen(200)
-    print 'listen dev on 5800'
+    print ('listen web on 5801')
 
     input_list = []
     input_list.append(server)
@@ -118,33 +44,112 @@ def dev_loop() :
             if s == server:
                 clientsock, clientaddr = server.accept()
                 input_list.append(clientsock)
-                print 'dev loop', clientaddr, "has connected"
+                g_sock_name[clientsock.fileno()] = 'test1'
+                g_sock_sock[clientsock.fileno()] = clientsock
+                print ('web loop', clientaddr, "has connected, fileno = ", clientsock.fileno())
                 break
 
-            data = s.recv(8)
-            if len(data) == 0:
-                del_dev_sock(s)
+            print ('web got data fileno = ', s.fileno())
+            try:
+                data = s.recv(4096)
+                print (data)
+            except:
+                pass
+            dlen = len(data)
+
+            if dlen == 0:
+                del g_sock_name[s.fileno()]
+                del g_sock_sock[s.fileno()]
                 input_list.remove(s)
                 s.close()
-                print 'dev loop socket close'
+                print ('web socket close')
                 break
 
+            name = g_sock_name[s.fileno()]
+            dev_sock = g_dev_socks[name]
+
+            #print ('-- send to dev, index = ', s.fileno(), 'len = ', dlen)
+            head = pack('<ccccII', b'd', b'0', b'0', b'0', s.fileno(), dlen)
+            dev_sock.sendall(head)
+            dev_sock.sendall(data)
+
+def parse_head(data) :
+    (t,n,n,n,i,l) = unpack('<cBBBII', data)
+    t = t.decode("utf-8")
+    return {'type':t, 'index':i, 'length':l}
+
+def dev_loop() :
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(('', 5800))
+    server.listen(200)
+    print ('listen dev on 5800')
+
+    input_list = []
+    input_list.append(server)
+
+    while True :
+        inputready, outputready, exceptready = select.select(input_list, [], [])
+        for s in inputready:
+            if s == server:
+                clientsock, clientaddr = server.accept()
+                input_list.append(clientsock)
+                print ('dev loop', clientaddr, "has connected, fileno = ", clientsock.fileno())
+                break
+
+            #print ('dev got data fileno = ', s.fileno())
+
+            try:
+                data = s.recv(12)
+            except:
+                pass
+
+            if not data or len(data) != 12:
+                del g_dev_socks[g_sock_name[s.fileno()]]
+                del g_sock_name[s.fileno()]
+                input_list.remove(s)
+                s.close()
+                print ('dev socket close ---------------')
+                break
+            
+            #print (data)
             head = parse_head(data)
-
-            if head.type == 'i' :
+            print (head)
+            
+            if head['type'] == 'i' :
                 name = s.recv(head['length'])
+                name = name.decode("utf-8")
+                g_sock_name[s.fileno()] = name
                 g_dev_socks[name] = s
-                print 'dev loop socket got name: ', name
+                print ('dev socket got name: ', name)
                 break
-
-            print 'dev loop unknow error'
-
+            
+            if head['type'] == 'd':
+                #print ('dev socket forward length', head['length'])
+                web_sock = g_sock_sock[head['index']]
+                rest = head['length']
+                while rest != 0 :
+                    fdata = s.recv(rest)
+                    if not fdata :
+                        print ("recv bug");
+                    rest = rest - len(fdata)
+                    try:
+                        web_sock.sendall(fdata)
+                    except:
+                        pass
+                    #print ('forward loop, got ', len(fdata), "rest ", rest)
+                break
+            
+            print ('dev loop unknow error --------------')
+            del g_dev_socks[g_sock_name[s.fileno()]]
+            del g_sock_name[s.fileno()]
+            input_list.remove(s)
+            s.close()
 
 
 if __name__ == '__main__':
 
-   devt = threading.Thread(target=dev_loop)
-   devt.start()
+    devt = threading.Thread(target=dev_loop)
+    devt.start()
 
-   server = TheServer('', 5801)
-   server.main_loop()
+    web_loop()
